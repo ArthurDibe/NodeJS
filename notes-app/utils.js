5ac1f9d318e72b809f5e4589ce83fef6e2dfde73
Arthur Dibe <arthurverissimo82@gmail.com>
2019-10-19 13:23:43 -0400

    created utils.js: added description and console.log.

---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+/*
+    AUTHOR: Dibe
+    DESCRIPTION:
+    This module holds some implementation
+    to be imported and be used by other 
+    modules for node.js projects !
+ */
+
+console.log('utils.js got accessed!');
