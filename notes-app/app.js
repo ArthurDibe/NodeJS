be2edc7ef93509dfce9e774a8f880963668dbb0d
Arthur Dibe <arthurverissimo82@gmail.com>
2019-10-19 14:16:18 -0400

    code app.js: created two variables (num1, num2) that hold numbers to be added via utils.add() and the result of the sum is assigned to the array of messages.

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,17 +6,21 @@
     into it. then once is done, closes the file
 */
 
-const data2 = require('./utils.js');      // import the utils.js module into appp.js module
+const utils = require('./utils.js');      // import the utils.js module into appp.js module
 const fs = require('fs');   // import fs object to write in files
 
 const data = 'This is a new line appended from app.js!\n';    // data to be appended
 
-// fs.writeFileSync('notes.txt','This file was created by Node.js!*');     // create and/or overwrite the file with a content
+let messages = [];  // create an empty array
+
+const num1 = 5;
+const num2 = 10;
+messages.push(data);
+messages.push(utils.message);
+messages.push(`The sum of ${num1} and ${num2} = ${utils.add(num1,num2)}`);
 
 let noteFile;   // needs to be declared outside in order to be read inside the error handling scope
-let messages = [];
-messages.push(data);
-messages.push(data2);
+// fs.writeFileSync('notes.txt','This file was created by Node.js!*');     // create and/or overwrite the file with a content
 
 try {
     noteFile = fs.openSync('notes.txt','a');  // option to open a file in append mode
