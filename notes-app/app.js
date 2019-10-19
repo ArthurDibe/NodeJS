aa5a9c19d36cb2a50a4988f184fe1c00b3a65aa3
Arthur Dibe <arthurverissimo82@gmail.com>
2019-10-19 16:22:08 -0400

    code app.js: testing validator by just checking an EMAIL and an URL

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
+const validator = require('validator');
 const notes = require('./notes.js');
 
 const noteMessage = "Edgar Allan Poe once said: Never more...";
 
 console.log(notes.getNotes(noteMessage));
+console.log();
+console.log(validator.isEmail('person@example.com'));  // validates a receiving email
+console.log(validator.isURL('www.google.com'));        // validates a receiving URL
