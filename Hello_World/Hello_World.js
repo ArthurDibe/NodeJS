0d8960373a1a599ce7757d43b5f65ba025c0ec2e
Arthur Dibe <arthurverissimo82@gmail.com>
2019-10-19 10:42:18 -0400

    Added code description at the top of the Hello_World.js file

--- Hello_World/Hello_World.js
+++ Hello_World/Hello_World.js
@@ -1,1 +1,8 @@
-console.log("Hello World of NodeJS!")
+/*This is a simple script just to run
+ * the first NodeJS code.
+ * Do not forget to install NodeJS first,
+ * then you can run this code in your
+ * machine by just simply enter in the
+ * terminal node Hello_World.*/
+
+console.log("Hello World of NodeJS!");
