492bd54e83b9eb15b19493decf4da912a077df12
Arthur Dibe <arthurverissimo82@gmail.com>
2019-10-19 13:44:54 -0400

    code app.js: created an array that hold messages, and the for loop to display the messages inside the array of messages

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,15 +9,19 @@
 require('./utils.js');      // import the utils.js module into appp.js module
 const fs = require('fs');   // import fs object to write in files
 
-const data = 'This is a new line appended!\n';    // data to be appended
+const data = 'This is a new line appended from app.js!\n';    // data to be appended
 
 // fs.writeFileSync('notes.txt','This file was created by Node.js!*');     // create and/or overwrite the file with a content
 
 let noteFile;   // needs to be declared outside in order to be read inside the error handling scope
+let messages = [];
+messages.push(data);
+messages.push(data2);
 
 try {
     noteFile = fs.openSync('notes.txt','a');  // option to open a file in append mode
-    fs.appendFileSync(noteFile,data,'utf-8');
+    for(let element in messages)
+        fs.appendFileSync(noteFile,element,'utf-8');
 } catch (err){
     /* Error */
 } finally {
