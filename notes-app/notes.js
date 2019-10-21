257dfb1f2591209f8a213165b41465cdcbb55356
Arthur Dibe <arthurverissimo82@gmail.com>
2019-10-21 14:52:32 -0400

    code notes.js:
    Implemented the definitions of the function removeNote, listNotes and readNote

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -13,24 +13,18 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-    return "Once Edgar Allan Poe said: \"Never more...\"";
-}
-
-
-
 const addNote = (title, body) =>
 {
     // retrieve an object or an empty array
     // pushing the results into the 
     const notes = loadNotes();  
 
-    // array filter method looks for all elements
-    // of an array and returns the ones who match
-    // a certain condition, and store the in a array
-    const duplicateNotes = notes.filter( (element)=> element.title.toLowerCase()===title.toLowerCase() );
-    
-    if(duplicateNotes.length === 0)
+    // find() built-in method returns the element that is found by the callback arrow function
+    const duplicated = notes.find( (element) => element.title.toLowerCase()===title.toLowerCase() );
+
+    // if there no duplicated element
+    // add the new note, else print error message
+    if(!duplicated)
     {
         notes.push({                
             title: title,
@@ -41,6 +35,62 @@ const addNote = (title, body) =>
     } else {
         console.log(chalk.yellow(`${title}`),chalk.yellow.inverse("ALREADY EXISTS!"));
     }
+}
+
+
+const removeNote = (title) =>
+{
+    const notes = loadNotes();
+
+    // filter() built-in method returns the elements that are not equal to the one specified by the user,
+    // the test is made by the anonymous callback arrow function
+    const notesToKeep = notes.filter( (element)=> element.title.toLowerCase() !== title.toLowerCase() );
+
+    // check if there was an element removed
+    if(notesToKeep.length !== notes.length) 
+    {
+        saveNotes(notesToKeep);
+        console.log(chalk.green(`${title}`),chalk.green.inverse("REMOVED!"));
+    } 
+    else {
+        console.log(chalk.yellow(`${title}`),chalk.yellow.inverse("NOT FOUND!"));
+    }  
+}
+
+
+const listNotes = () =>
+{
+    console.log("--------------------------------------------------------------");
+    console.log(chalk.cyan.underline("List of your notes:"));
+    const list = loadNotes();
+    if(list.length === 0)
+    {
+        console.log(chalk.cyan.inverse("THERE IS NOTHING TO SHOW. ZERO NOTES"));
+    } 
+    else {
+        for(let i in list) {
+            console.log(chalk.magenta(`Note #${parseInt(i)+1}: `),`${list[i].title}`);
+        }
+        console.log("--------------------------------------------------------------");
+    }
+}
+
+
+const readNote = (title) =>
+{
+    console.log("--------------------------------------------------------------");
+    const notes = loadNotes();
+    const noteFound = notes.find( (element)=> element.title.toLowerCase() === title.toLowerCase() );
+    if(noteFound)
+    {
+        console.log(chalk.green(`* Title: `), `${noteFound.title}`);
+        console.log(chalk.green(`* Description: `), `${noteFound.body}`);
+    } 
+    else {
+        console.log(chalk.cyan(`${title}`), chalk.cyan.inverse(` NOT FOUND`));
+    }
+    console.log("--------------------------------------------------------------");
+
 }
 
 
@@ -65,25 +115,9 @@ const loadNotes = () =>
 }
 
 
-const removeNote = (title) =>
-{
-    const notes = loadNotes();
-
-    // array of notes to keep
-    const notesToKeep = notes.filter( (element) => element.title.toLowerCase()!==title.toLowerCase() );
-
-    // check if the array of notes to keep has the same length as the original
-    if(notesToKeep.length !== notes.length) {
-        saveNotes(notesToKeep);
-        console.log(chalk.green(`${title}`),chalk.green.inverse("REMOVED!"));
-    } else {
-        console.log(chalk.yellow(`${title}`),chalk.yellow.inverse("NOT FOUND!"));
-    }  
-}
-
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
-    loadNotes: loadNotes,
-    removeNote: removeNote
+    listNotes: listNotes,
+    removeNote: removeNote,
+    readNote: readNote
 };
